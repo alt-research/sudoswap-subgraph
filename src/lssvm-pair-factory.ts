2cc53a51c17cf6a73a2b38d3686e40273966d216
micovi <micleaovidiul@gmail.com>
2022-10-28 12:17:03 +0700

    fix abi read

--- src/lssvm-pair-factory.ts
+++ src/lssvm-pair-factory.ts
@@ -17,13 +17,168 @@ import { NewPair, Pair, NFT, DailyETHPairStat, DailyETHPoolStat, DailyETHProtoco
 import { LSSVMPairEnumerableETH } from "../generated/templates"
 import { plusBigInt, updatePairAttributesIfMissing } from "./utilities"
 import { ethers } from "ethers"
-import chains from "../chains.json";
-import ERC721ABI from "../abis/ERC721.json";
 
-const CHAIN = chains[9990];
+const ERC721ABI = [
+  {
+    "anonymous": false,
+    "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, {
+      "indexed": true,
+      "internalType": "address",
+      "name": "approved",
+      "type": "address"
+    }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, {
+      "indexed": true,
+      "internalType": "address",
+      "name": "operator",
+      "type": "address"
+    }, { "indexed": false, "internalType": "bool", "name": "approved", "type": "bool" }],
+    "name": "ApprovalForAll",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, {
+      "indexed": true,
+      "internalType": "address",
+      "name": "to",
+      "type": "address"
+    }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, {
+      "internalType": "uint256",
+      "name": "tokenId",
+      "type": "uint256"
+    }], "name": "approve", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "internalType": "uint256", "name": "balance", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "getApproved",
+    "outputs": [{ "internalType": "address", "name": "operator", "type": "address" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, {
+      "internalType": "address",
+      "name": "operator",
+      "type": "address"
+    }],
+    "name": "isApprovedForAll",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "ownerOf",
+    "outputs": [{ "internalType": "address", "name": "owner", "type": "address" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, {
+      "internalType": "address",
+      "name": "to",
+      "type": "address"
+    },
+    { "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, {
+      "internalType": "address",
+      "name": "to",
+      "type": "address"
+    },
+    { "internalType": "uint256", "name": "tokenId", "type": "uint256" }, {
+      "internalType": "bytes",
+      "name": "data",
+      "type": "bytes"
+    }], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "operator", "type": "address" }, {
+      "internalType": "bool",
+      "name": "_approved",
+      "type": "bool"
+    }], "name": "setApprovalForAll", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" }],
+    "name": "supportsInterface",
+    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "tokenURI",
+    "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, {
+      "internalType": "address",
+      "name": "to",
+      "type": "address"
+    }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "transferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
 
 const provider = new ethers.providers.JsonRpcProvider({
-  url: CHAIN.rpcUrls.default,
+  url: "https://acadia2-alt-producer-archive-rpc.altresearch.xyz/",
 });
 
 export async function handleCreatePairETH(
